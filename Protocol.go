2fbaa6413bb3fbd560a9b302c767db089e088e08
Christopher Lillthors <lillt@kth.se>
2014-05-29 23:40:02 +0200

    some stuff

--- Protocol.go
+++ Protocol.go
@@ -3,9 +3,17 @@ package Protocol
 type Menus map[string]Menu
 
 type Menu struct {
-	Menu []string `json:menu`
+	Menu    []string `json:"menu"`
+	Login   []string `json:"log-in"`
+	Invalid []string `json:"invalid-input"`
 }
 
 type MenuConfig struct {
 	Menus
+}
+
+type Message struct {
+	Number   uint16 `codec:",omitempty"` //3byte
+	Pass     uint16 `codec:",omitempty"` //3 byte
+	LoggedIn bool   `codec:",omitempty"` // 1 byte
 }
