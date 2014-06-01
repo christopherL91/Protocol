74d7f139887a548dbf5df4f411f8b5905320c23a
Christopher Lillthors <lillt@kth.se>
2014-06-01 21:51:16 +0200

    lgmt

--- Protocol.go
+++ Protocol.go
@@ -1,10 +1,33 @@
 package Protocol
 
+import (
+	"regexp"
+)
+
+var (
+	LoginStatusCode    uint16 = 1
+	LogoutStatusCode   uint16 = 2
+	DepositStatusCode  uint16 = 3
+	WithdrawStatusCode uint16 = 4
+	BalanceStatusCode  uint16 = 5
+
+	ResponseOK              uint16 = 0
+	ResponseAlreadyLoggedIn uint16 = 1
+	ResponseNotAccepted     uint16 = 2
+	ResponseMoneyProblem    uint16 = 3
+
+	CardnumberTest = regexp.MustCompile(`^(\d){4}$`) //4 digits.
+	PassnumberTest = regexp.MustCompile(`^(\d){2}$`) //2 digits.
+	MoneyTest      = regexp.MustCompile(`^(\d+)$`)   //at least one digit.
+	ScratchTest    = regexp.MustCompile(`^(\d+)$`)   //at least one digit.
+)
+
 type Menus map[string]Menu
 
 type Menu struct {
 	Menu    []string `json:"menu"`
 	Login   []string `json:"log-in"`
+	Banner  string   `json:"banner"`
 	Invalid []string `json:"invalid-input"`
 }
 
@@ -13,7 +36,9 @@ type MenuConfig struct {
 }
 
 type Message struct {
-	Number   uint16 `codec:",omitempty"` //3byte
-	Pass     uint16 `codec:",omitempty"` //3 byte
-	LoggedIn bool   `codec:",omitempty"` // 1 byte
+	_struct bool   `codec:",toarray"`
+	Code    uint16 //3 byte
+	Number  uint16 //3byte
+	Payload uint16 //3 byte
+	//Total packet weight: 9byte.
 }
